7892ec209842cf6efe014c4db41836ca72ac3969
Maaxboon <maaxwelokoth@gmail.com>
2024-11-14 15:40:26 +0300

    Explains function declaration and calling

--- functions/draw.js
+++ functions/draw.js
@@ -5,3 +5,7 @@ function draw() {
 
 // call the function draw
 draw();
+
+/*
+Function declaration: function draw(){...} defines a function draw. Inside the curly braces you put the code you want to run whenever the code is called.
+Calling the function: draw(); calls the function, meaning JavaScript goes to the draw function and executes the code inside it */
