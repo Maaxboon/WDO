022e284a1cbb05787de6297de7043860095e2c16
Maaxboon <maaxwelokoth@gmail.com>
2024-11-14 19:45:27 +0300

    Adds first three possiblities (player 1 wins)

---
+++ JavaScript/conditionals/practiceActivity1.js
@@ -0,0 +1,25 @@
+let rock = "rock";
+let scissors = "scissors";
+let paper = "paper";
+
+// these two variables will be changed to test the conditional
+let firstPlayerChoice;
+let secondPlayerChoice;
+
+// First three possibilities (Player one wins)
+// First possibility: Player 1 chooses paper and Player 2 chooses rock (Player 1 wins)
+let firstPossibility = firstPlayerChoice == paper && secondPlayerChoice == rock;
+
+// Second possibility: Player 1 chooses rock and player 2 chooses scissors (player 1 wins)
+let secondPossibility =
+  firstPlayerChoice == rock && secondPlayerChoice == scissors;
+
+//   Third possibility: player 1 chooses scissors and player 2 chooses paper (player 1 wins)
+let thirdPossibility =
+  fifthPossibility == scissors && secondPlayerChoice == paper;
+
+let fourthPossibility;
+let fifthPossibility;
+let sixthPossibility;
+
+let seventhPossibility;
