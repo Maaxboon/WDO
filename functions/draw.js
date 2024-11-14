bcd4c9a4d6aa5d591a965ea9c1a1cd46aad541bf
Maaxboon <maaxwelokoth@gmail.com>
2024-11-14 19:36:58 +0300

    Calls draw function

--- functions/draw.js
+++ functions/draw.js
@@ -7,5 +7,6 @@ function draw() {
 draw();
 
 /*
-Function declaration: function draw(){...} defines a function draw. Inside the curly braces you put the code you want to run whenever the code is called.
-Calling the function: draw(); calls the function, meaning JavaScript goes to the draw function and executes the code inside it */
+1. Function declaration: function draw(){...} defines a function draw. Inside the curly braces you put the code you want to run whenever the code is called.
+
+2. Calling the function: draw(); calls the function, meaning JavaScript goes to the draw function and executes the code inside it */
